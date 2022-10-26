6e15f7b130ec10cdd17716511f0bbb421368b87d
CadeMichael <cade.lueker@gmail.com>
2022-10-26 13:16:51 -0600

    adding testing functionality to rails and npm to node

--- lua/node.lua
+++ lua/node.lua
@@ -30,3 +30,23 @@ vim.api.nvim_create_user_command("LoadNode",
   function(opts)
     LoadNode(opts.args)
   end, { nargs = 1 })
+
+-- run npm commands
+function RunNpm()
+  -- get root dir ie with package.json
+  local root = vim.lsp.buf.list_workspace_folders()[1]
+  -- build full command including dir change
+  local go_root = "cd " .. root .. " && "
+  local command = "npm " .. vim.fn.input("npm => ", "")
+  -- create blank window
+  vim.cmd ":wincmd n"
+  vim.cmd ":wincmd J"
+  -- run npm command
+  vim.fn.termopen(go_root .. command)
+end
+
+-- create user command
+vim.api.nvim_create_user_command("RunNpm",
+  function()
+    RunNpm()
+  end, {})
--- lua/rails.lua
+++ lua/rails.lua
@@ -49,3 +49,85 @@ vim.api.nvim_create_user_command("LoadIRB",
   function(opts)
     LoadIRB(opts.args) -- positions
   end, { nargs = 1 })
+
+local function getRoot()
+  local gemfile = vim.fn.findfile('Gemfile', ';.')
+  local file_tab = vim.fn.split(gemfile, '/')
+  table.remove(file_tab, #file_tab)
+  local root = table.concat(file_tab, '/')
+  return root
+end
+
+local function runProjRails ()
+  local root = getRoot()
+  local rails
+  if #root > 2 then
+    root = '/' .. root
+    rails = root .. '/bin/rails'
+  else
+    rails = 'bin/rails'
+  end
+  return { rails, root }
+end
+
+function RailsTestFile()
+  -- get variables from helper func
+  local vars = runProjRails()
+  local rails = vars[1]
+  local root = vars[2]
+  -- messages to user
+  print("Run a Test File...\n")
+  print("Project root => " .. root .. "\n")
+  -- get current file (should be a test file)
+  local cur_file = vim.api.nvim_buf_get_name(0)
+  -- helper function to iterate through dirs of the cur file 
+  -- and check if there is a 'test/' dir in its path
+  local function splitFile(file, dir)
+    -- split input file
+    local file_tab = vim.fn.split(file, '/')
+    -- keep track of deleted dirs to prevent delete nil loop
+    local cnt = #file_tab
+    -- loop through dirs removing until you hit 'test'
+    while (file_tab[1] ~= dir and cnt ~= 0) do
+      cnt = cnt - 1
+      table.remove(file_tab, 1)
+    end
+    -- if file_tab is <= 1 then it isn't in 'test' dir
+    if #file_tab <= 1 then
+      -- bad file value
+      return nil
+    end
+    -- current file is in 'test/' compose and return
+    return table.concat(file_tab, "/")
+  end
+  -- get relative (to root dir) path of test file
+  local test_file = splitFile(cur_file, "test")
+  -- see if file is in 'test/' dir
+  if test_file == nil then
+    print('not a test file !')
+    -- break out of function
+    return
+  end
+  -- compose command
+  local command = rails .. " test " .. test_file
+  -- create new window
+  vim.cmd(":wincmd n")
+  vim.cmd(":wincmd J")
+  -- rails exe is not in cur dir
+  if #rails ~= 9 then
+    -- set dir before command
+    command = 'cd ' .. root .. ' && ' .. command
+    print(test_file)
+    vim.fn.termopen('cd ' .. root .. ' && ' .. command)
+  -- rails exe is in current dir
+  else
+    print(test_file)
+    vim.fn.termopen(command)
+  end
+end
+
+-- create user command
+vim.api.nvim_create_user_command("RailsTestFile",
+  function()
+    RailsTestFile()
+  end, {})
