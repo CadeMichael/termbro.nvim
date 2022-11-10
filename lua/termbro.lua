0f64f2d4183007308ce0585d7f872e61d1941415
CadeMichael <cade.lueker@gmail.com>
2022-11-10 13:06:41 -0700

    moving zig to separate plugin

--- lua/termbro.lua
+++ lua/termbro.lua
@@ -12,7 +12,6 @@ local node = require('node')
 local python = require('python')
 local rails = require('rails')
 local term = require('term')
-local zig = require('zig')
 
 termbro.django = django
 termbro.lua = lua
@@ -20,7 +19,6 @@ termbro.node = node
 termbro.python = python
 termbro.rails = rails
 termbro.term = term
-termbro.zig = zig
 
 -- return complete plugin
 return termbro
